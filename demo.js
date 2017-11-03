f21e4ae7c69c749ebf14e5ee33a66d62ad714187
Ion D. Filho <iondrimba@gmail.com>
2017-11-03 17:56:52 -0200

    playing video

--- demo.js
+++ demo.js
@@ -1,3 +1,39 @@
-window.addEventListener('DOMContentLoad', function () {
-  console.log('HI');
+var scrollY = 0;
+var videoElement = null;
+
+window.addEventListener('scroll', setScrollY);
+
+function setScrollY() {
+  scrollY = window.pageYOffset || window.scrollY;
+}
+
+function displayScrollPos() {
+  var offsetElt = offset(videoElement);
+
+  if (scrollY >= offsetElt.top) {
+    window.removeEventListener('scroll', setScrollY);
+
+    videoElement.play();
+
+    return false;
+  }
+  requestAnimationFrame(displayScrollPos);
+};
+
+
+function offset(elt) {
+  var rect = elt.getBoundingClientRect(), bodyElt = document.body;
+
+  return {
+    top: rect.top + bodyElt.scrollTop,
+    left: rect.left + bodyElt.scrollLeft
+  }
+}
+
+window.addEventListener('DOMContentLoaded', function () {
+
+  videoElement = document.querySelector('video');
+
+  requestAnimationFrame(displayScrollPos);
+
 });
